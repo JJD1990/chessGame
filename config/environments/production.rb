d5a96b24306ed5a8a68ae45bddd36cf5c88eee53
jjd1990 <jack.douglas@rocketmail.com>
2023-03-26 17:22:42 +0100

    Allow host through environment variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-  config.hosts << "chessgame-c3au.onrender.com"
+  config.hosts << ENV["ALLOWED_HOST"] if ENV["ALLOWED_HOST"].present?
 
 
   # Mount Action Cable outside main process or domain.
