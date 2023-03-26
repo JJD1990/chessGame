a7dc95e378ae3c88c18683a4544fe628f622eec5
jjd1990 <jack.douglas@rocketmail.com>
2023-03-26 17:40:07 +0100

    development config added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.hosts << "gerbil.chess-application.c66.me"
+  config.hosts << "chessgame-c3au.onrender.com"
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
